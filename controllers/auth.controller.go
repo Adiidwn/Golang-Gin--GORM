03a0981f57baa2288d735275f50368731eb43e43
adiw <adindo48@gmail.com>
2024-04-18 23:09:10 +0700

    auth

---
+++ controllers/auth.controller.go
@@ -0,0 +1,199 @@
+package controllers
+
+import (
+	"fmt"
+	"golang-gorm/initializers"
+	"golang-gorm/models"
+	"golang-gorm/utils"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+	"gorm.io/gorm"
+)
+
+type AuthController struct {
+	DB *gorm.DB
+}
+
+func NewAuthController(DB *gorm.DB) AuthController {
+	return AuthController{DB}
+}
+
+// SignUp User
+func (ac *AuthController) SignUpUser(ctx *gin.Context) {
+	var payload models.SignUpInput
+
+	if err := ctx.ShouldBindJSON(&payload); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"status": "fail", "message": err.Error()})
+		return
+	}
+
+	if payload.Password != payload.PasswordConfirm {
+		ctx.JSON(http.StatusBadRequest, gin.H{"status": "fail", "message": "Passwords do not match"})
+		return
+	}
+
+	hashedPassword, err := utils.HashPassword(payload.Password)
+	if err != nil {
+		ctx.JSON(http.StatusBadGateway, gin.H{"status": "error", "message": err.Error()})
+		return
+	}
+
+	now := time.Now()
+	newUser := models.User{
+		Name:      payload.Name,
+		Email:     strings.ToLower(payload.Email),
+		Password:  hashedPassword,
+		Role:      "user",
+		Verified:  true,
+		Photo:     payload.Photo,
+		Provider:  "local",
+		CreatedAt: now,
+		UpdatedAt: now,
+	}
+
+	data, _ := http.NewRequest("GET", viper.GetString("DB_POST")+"/users", nil)
+	fmt.Println("asdasd", data)
+	result := ac.DB.Create(&newUser)
+
+	if result.Error != nil && strings.Contains(result.Error.Error(), "duplicate key value violates unique") {
+		ctx.JSON(http.StatusConflict, gin.H{"status": "fail", "message": "User with that email already exists"})
+		return
+	} else if result.Error != nil {
+		ctx.JSON(http.StatusBadGateway, gin.H{"status": "error", "message": "Something bad happened"})
+		return
+	}
+
+	userResponse := &models.UserResponse{
+		ID:        newUser.ID,
+		Name:      newUser.Name,
+		Email:     newUser.Email,
+		Photo:     newUser.Photo,
+		Role:      newUser.Role,
+		Provider:  newUser.Provider,
+		CreatedAt: newUser.CreatedAt,
+		UpdatedAt: newUser.UpdatedAt,
+	}
+	ctx.JSON(http.StatusCreated, gin.H{"status": "success", "data": gin.H{"user": userResponse}})
+}
+
+func (ac *AuthController) SignInUser(ctx *gin.Context) {
+	var payload models.SignInInput
+
+	if err := ctx.ShouldBindJSON(&payload); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"status": "fail", "message": err.Error()})
+		return
+	}
+
+	var user models.User
+	result := ac.DB.First(&user, "email = ?", strings.ToLower(payload.Email))
+	if result.Error != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"status": "fail", "message": "Invalid email or Password"})
+		return
+	}
+
+	if err := utils.VerifyPassword(user.Password, payload.Password); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"status": "fail", "message": "Invalid email or Password"})
+		return
+	}
+
+	config, _ := initializers.LoadConfig(".")
+
+	// Generate Tokens
+	access_token, err := utils.CreateToken(config.AccessTokenExpiresIn, user.ID, config.AccessTokenPrivateKey)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"status": "fail", "message": err.Error()})
+		return
+	}
+
+	refresh_token, err := utils.CreateToken(config.RefreshTokenExpiresIn, user.ID, config.RefreshTokenPrivateKey)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"status": "fail", "message": err.Error()})
+		return
+	}
+
+	ctx.SetCookie("access_token", access_token, config.AccessTokenMaxAge*60, "/", "localhost", false, true)
+	ctx.SetCookie("refresh_token", refresh_token, config.RefreshTokenMaxAge*60, "/", "localhost", false, true)
+	ctx.SetCookie("logged_in", "true", config.AccessTokenMaxAge*60, "/", "localhost", false, false)
+
+	ctx.JSON(http.StatusOK, gin.H{"status": "success", "access_token": access_token, "data": user})
+}
+
+// Refresh Access Token
+func (ac *AuthController) RefreshAccessToken(ctx *gin.Context) {
+	message := "could not refresh access token"
+
+	cookie, err := ctx.Cookie("refresh_token")
+
+	if err != nil {
+		ctx.AbortWithStatusJSON(http.StatusForbidden, gin.H{"status": "fail", "message": message})
+		return
+	}
+
+	config, _ := initializers.LoadConfig(".")
+
+	sub, err := utils.ValidateToken(cookie, config.RefreshTokenPublicKey)
+	if err != nil {
+		ctx.AbortWithStatusJSON(http.StatusForbidden, gin.H{"status": "fail", "message": err.Error()})
+		return
+	}
+
+	var user models.User
+	result := ac.DB.First(&user, "id = ?", fmt.Sprint(sub))
+	if result.Error != nil {
+		ctx.AbortWithStatusJSON(http.StatusForbidden, gin.H{"status": "fail", "message": "the user belonging to this token no logger exists"})
+		return
+	}
+
+	access_token, err := utils.CreateToken(config.AccessTokenExpiresIn, user.ID, config.AccessTokenPrivateKey)
+	if err != nil {
+		ctx.AbortWithStatusJSON(http.StatusForbidden, gin.H{"status": "fail", "message": err.Error()})
+		return
+	}
+
+	ctx.SetCookie("access_token", access_token, config.AccessTokenMaxAge*60, "/", "localhost", false, true)
+	ctx.SetCookie("logged_in", "true", config.AccessTokenMaxAge*60, "/", "localhost", false, false)
+
+	ctx.JSON(http.StatusOK, gin.H{"status": "success", "access_token": access_token})
+}
+
+func (ac *AuthController) LogoutUser(ctx *gin.Context) {
+	ctx.SetCookie("access_token", "", -1, "/", "localhost", false, true)
+	ctx.SetCookie("refresh_token", "", -1, "/", "localhost", false, true)
+	ctx.SetCookie("logged_in", "", -1, "/", "localhost", false, false)
+
+	ctx.JSON(http.StatusOK, gin.H{"status": "success"})
+}
+func (ac *AuthController) GetData(ctx *gin.Context) {
+	var payload models.User
+	currentUser, _ := ctx.Get("currentUser")
+
+	if user, ok := currentUser.(models.User); ok {
+		userID := user.ID
+		fmt.Println("User ID:", userID)
+		if err := ac.DB.First(&payload, userID).Error; err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"status": "fail", "message": err.Error()})
+		}
+		datas := models.UserData{
+			ID:       payload.ID,
+			Name:     payload.Name,
+			Email:    payload.Email,
+			Role:     payload.Role,
+			Verified: payload.Verified,
+			Photo:    payload.Photo,
+			Provider: payload.Provider,
+		}
+
+		ctx.JSON(http.StatusOK, gin.H{"status": "success", "datas": datas})
+	} else {
+		// Handle the case where currentUser is not of type models.User
+		fmt.Println("currentUser is not of type models.User")
+	}
+	fmt.Println("currentUser", currentUser)
+
+	fmt.Println("payload", payload)
+
+}
